762cbd3e8bc0e5a195d5fcbc026b6f536f27ab10
xiote <xiote@nate.com>
2020-04-01 18:05:32 +0900

    Create aptdetail.go

---
+++ models/aptdetail.go
@@ -0,0 +1,17 @@
+package models
+
+type AptDetail struct {
+	AptNo       int
+	AptType string
+  AptName string
+  CortarNo string
+  Addr string
+  City string
+  Dvsn string
+  Sec string
+  DtlAddr string
+  TotHsehCnt int
+  DongCount int
+  PlanTypeCount int
+  OwnerInfoApiUrl string
+}
