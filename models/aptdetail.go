20b0d5a3d34477940db1be0d2d1fd1c68d06890c
xiote <xiote@nate.com>
2020-04-04 00:38:40 +0900

    Update aptdetail.go

--- models/aptdetail.go
+++ models/aptdetail.go
@@ -3,17 +3,16 @@ package models
 type AptDetail struct {
 	AptNo       int
 	AptType string
-  AptName string
-  CortarNo string
-  Addr string
-  City string
-  Dvsn string
-  Sec string
-  DtlAddr string
-  TotHsehCnt int
-  DongCount int
-  PlanTypeCount int
-  OwnerInfoApiUrl string
-	  XmlOwnerInfoApiUrl string
-
+  	AptName string
+  	CortarNo string
+  	Addr string
+  	City string
+  	Dvsn string
+  	Sec string
+  	DtlAddr string
+	TotHsehCnt int
+	DongCount int
+  	PlanTypeCount int
+  	OwnerInfoApiUrl string
+	XmlOwnerInfoApiUrl string
 }
